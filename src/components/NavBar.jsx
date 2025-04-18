5c715a918079dcbb206ea1b7a0d9f46ae55a7e30
Brayan Stiben Baeza Velasquez <brayanbaeza10@gmail.com>
2025-04-18 14:01:08 -0500

    Recreo componente NavBar perdido

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">E&R Fashion</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Inicio</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/categories">Categorías</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/ropa-dama">Dama</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/ropa-caballero">Caballero</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contacto">Contacto</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
